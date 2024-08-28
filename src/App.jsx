d37613a8ca45c4c4846eecb6c0baf428fb7d5d25
AllredW <allrewil@gmail.com>
2024-08-28 18:42:03 -0400

    built up render-able page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import Header from "./components/Header";
-import Section from "./components/Section";
-import Navbar from "./components/Navbar";
+// Bringing in the required import from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
+import Nav from './components/Nav';
 
 function App() {
+  // The Outlet component will conditionally swap between the different pages according to the URL
   return (
-    <div>
-      <Navbar />
-      <Header />
-      <Section />
-    </div>
+    <>
+      <Nav />
+      <Outlet />
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,41 @@
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+// Bringing in the pages the router will use to conditionally show the appropriate views
+import App from './App';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import AboutMe from './pages/AboutMe';
+
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'profile/:id',
+        element: <Portfolio />,
+      },
+      {
+        path: 'about',
+        element: <AboutMe />,
+      },
+    ],
+  },
+]);
+
+// Render the RouterProvider component
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);

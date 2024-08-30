e3e8b80081bac632327438079c08f58d3c839cb6
AllredW <allrewil@gmail.com>
2024-08-30 16:17:44 -0400

    updated README

--- README.md
+++ README.md
@@ -1,2 +1,116 @@
 # Portfolio-2.0
-This is a portfolio of my work in coding, with links to relevant information.
+
+<div align="center">
+
+  <!-- Add badges using the following format: -->
+  <!-- ![Name](urlToShieldHere)(urlToGithubHere) -->
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![Contributors](https://img.shields.io/github/contributors/AllredW/https://github.com/AllredW/Portfolio-2.0.svg?style=plastic&logo=appveyor)](https://github.com/AllredW/https://github.com/AllredW/Portfolio-2.0/graphs/contributors)
+[![Issues](https://img.shields.io/github/issues/AllredW/https://github.com/AllredW/Portfolio-2.0.svg?style=plastic&logo=appveyor)](https://github.com/AllredW/https://github.com/AllredW/Portfolio-2.0/issues)
+[![LinkedIn](https://img.shields.io/badge/-LinkedIn-black.svg?style=plastic&logo=appveyor&logo=linkedin&colorB=555)](https://linkedin.com/in/in/william-allred-6a2a522b3/)
+
+</div>
+
+  <h3 align="center">Portfolio 2.0</h3>
+
+  <p align="center">
+ <br />
+    <a href="https://github.com/AllredW/https://github.com/AllredW/Portfolio-2.0"><strong>Explore the docs »</strong></a>
+    <br />
+    <br />
+    ·
+    <a href="https://github.com/AllredW/https://github.com/AllredW/Portfolio-2.0/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/AllredW/https://github.com/AllredW/Portfolio-2.0/issues">Request Feature</a>
+
+  </p>
+</div>
+
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#roadmap">Roadmap</a></li>
+    <li><a href="#contributing">Contributing</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#acknowledgments">Acknowledgments</a></li>
+  </ol>
+</details>
+
+<!-- ABOUT THE PROJECT -->
+
+## About The Project
+
+This is a portfolio of my work in coding, with links to relevant information. It is an updated version of a previous portfolio page I had created, which incorportates more recent languages and coding practices which I have learned since creating the previous version.
+   
+### Built With
+
+<div align="center">
+
+[![Javascript](https://img.shields.io/badge/Language-JavaScript-ff0000?style=plastic&logo=JavaScript&logoWidth=10)](https://javascript.info/)
+[![HTML](https://img.shields.io/badge/Language-HTML/CSS-ff8000?style=plastic&logo=HTML5&logoWidth=10)](https://html.com/)
+[![Node.js](https://img.shields.io/badge/Framework-Node.js-ff0000?style=plastic&logo=Node.js&logoWidth=10)](https://nodejs.org/en/)
+[![Express](https://img.shields.io/badge/Framework-Express-80ff00?style=plastic&logo=Express&logoWidth=10)](https://expressjs.com/)
+[![npm](https://img.shields.io/badge/Tools-npm-ff0000?style=plastic&logo=npm&logoWidth=10)](https://www.npmjs.com/)
+[![VS Code](https://img.shields.io/badge/IDE-VSCode-ff0000?style=plastic&logo=VisualStudioCode&logoWidth=10)](https://code.visualstudio.com/docs)
+
+</div>
+
+
+## Getting Started
+
+This app is fully functional and live at:
+
+
+If you would like to run the app locally, follow the below installation instructions
+
+### Installation
+
+1. Clone the repo
+   git clone https://github.com/AllredW/Portfolio-2.0.git
+
+<!-- CONTRIBUTING -->
+
+## Contributing
+
+Contributions are what make the open source community such an amazing place to learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+If you have a suggestion that would make this better, please fork the repo and create a pull request. You can also simply open an issue with the tag "enhancement".
+Don't forget to give the project a star! Thanks again!
+
+1. Fork the Project
+2. Create your Feature Branch (git checkout -b feature/AmazingFeature)
+3. Commit your Changes (git commit -m 'Add some AmazingFeature')
+4. Push to the Branch (git push origin feature/AmazingFeature)
+5. Open a Pull Request
+
+<!-- LICENSE -->
+
+## License
+
+This project is licensed under the MIT license.
+
+See LICENSE.txt for more information.
+
+<!-- CONTACT -->
+
+## Contact
+
+[William Allred](https://github.com/AllredW) - allrewil@gmail.com
+
+[Github Repository](https://github.com/AllredW/https://github.com/AllredW/Portfolio-2.0)
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,36 +1,123 @@
-// Importing NodeMailer, to send email from input
-// let transporter = nodemailer.createTransport(transport[defaults])
+import { useState } from 'react';
+import { validateEmail } from "../utils/helpers";
 
 export default function ContactPage() {
-  var message = {
-    from: `${emailAddress}`,
-    to: "allrewil@gmail.com",
-    subject: `Message from Portfolio 2.0, from ${emailName}`,
-    text: `${emailBody}`,
-  };
+  const [emailAddress, setEmailAddress] = useState("");
+  const [emailName, setEmailName] = useState("");
+  const [emailBody, setEmailBody] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const sendEmail = () => {
-    if (!emailName) {
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputName = target.name;
+    const inputValue = target.value;
 
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputName === "emailAddress") {
+      setEmailAddress(inputValue);
+    } else if (inputName === "emailName") {
+      setEmailName(inputValue);
+    } else {
+      setEmailBody(inputValue);
     }
   };
-    return (
-      <>
-      <div className="pageBody">
-      <h2>Contact Me</h2>
-      <div className="input-group mb-3">
-  <input type="text" className="form-control" id="emailName" placeholder="Name" aria-label="Name" aria-describedby="basic-addon1" />
-</div>
 
-<div className="input-group mb-3">
-  <input type="text" className="form-control" id="emailAddress" placeholder="Email" aria-label="Email" aria-describedby="basic-addon2" />
-</div>
+  const handleFormSubmit = (e) => {
 
-<div className="input-group">
-  <textarea className="form-control" id="emailBody" placeholder="Your text here" aria-label="With textarea"></textarea>
-</div>
-<button type="button" onClick={() => sendEmail(`${emailName}, ${emailAddress}, ${emailBody}`)} className="btn btn-secondary" id="sendButton">Send</button>
-</div>
-      </>
-    );
-  }
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    if (!validateEmail(emailAddress) || !emailName || !emailBody) {
+      setErrorMessage("Something isn't right. Please check and try again.");
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
+    }
+
+    // If everything goes according to plan, we want to clear out the input after a successful registration.
+    setUserName("");
+    setPassword("");
+    setEmail("");
+  };
+
+  // //Nodemailer: Create a transporter object
+  // const transporter = nodemailer.createTransport({
+  //   host: "live.smtp.mailtrap.io",
+  //   port: 587,
+  //   secure: false, // use SSL
+  //   auth: {
+  //     user: "1a2b3c4d5e6f7g",
+  //     pass: "1a2b3c4d5e6f7g",
+  //   },
+  // });
+
+  // // Configure the mailoptions object
+  // const mailOptions = {
+  //   from: `${emailAddress}`,
+  //   to: "allrewil@gmail.com",
+  //   subject: `Email from ${emailName}, from your Portfolio Page!`,
+  //   text: `${emailBody}`,
+  // };
+
+  // // Send the email
+  // transporter.sendMail(mailOptions, function (error, info) {
+  //   if (error) {
+  //     console.log(Error, error);
+  //   } else {
+  //     console.log("Email sent: " + info.response);
+  //   }
+  // });
+
+  return (
+    <>
+      <form className="pageBody" onSubmit={handleFormSubmit}>
+        <h2>Contact Me</h2>
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            id="emailName"
+            placeholder="Name"
+            aria-label="Name"
+            aria-describedby="basic-addon1"
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            id="emailAddress"
+            placeholder="Email"
+            aria-label="Email"
+            aria-describedby="basic-addon2"
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <div className="input-group">
+          <textarea
+            className="form-control"
+            id="emailBody"
+            placeholder="Your text here"
+            aria-label="With textarea"
+            onChange={handleInputChange}
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          // onClick={() =>
+          //   sendEmail(`${emailName}, ${emailAddress}, ${emailBody}`)
+          // }
+          className="btn btn-secondary"
+          id="sendButton"
+        >
+          Send
+        </button>
+      </form>
+    </>
+  );
+}
